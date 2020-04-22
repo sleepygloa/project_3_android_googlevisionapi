c4c98e0eda18fc80d8451c1278d372a40bf3d45a
sleepygloa <sleepygloa@gmail.com>
2020-04-22 23:51:03 +0900

    study dialog flagment

--- app/src/main/java/com/sleepygloa/vision/api/MainActivity.kt
+++ app/src/main/java/com/sleepygloa/vision/api/MainActivity.kt
@@ -2,11 +2,22 @@ package com.sleepygloa.vision.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupListener();
+    }
+
+
+    private fun setupListener(){
+
+        upload_image.setOnClickListener(){
+            UploadChooser().show(supportFragmentManager, "")
+        }
     }
 }
---
+++ app/src/main/java/com/sleepygloa/vision/api/uploadChooser.kt
@@ -0,0 +1,22 @@
+package com.sleepygloa.vision.api
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class UploadChooser : BottomSheetDialogFragment (){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.upload_chooser, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
